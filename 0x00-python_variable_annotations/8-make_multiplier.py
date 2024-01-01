fcb191594eff77a6359ab7fa2a82757f0e1641f6
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-01 16:20:20 +0100

    documenatation fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,8 @@ from collections.abc import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """a type-annotated function make_multiplier that takes a float
+    """
+    a type-annotated function make_multiplier that takes a float
     multiplier as argument and returns a function that multiplies a
     float by multiplier.
     """
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Annotate the below function’s parameters and return
+"""
+Annotate the below function’s parameters and return
 values with the appropriate types
 """
 from typing import Sequence, Iterable, List
