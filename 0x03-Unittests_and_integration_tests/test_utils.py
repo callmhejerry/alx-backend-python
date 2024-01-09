04d6e111bf6574ff7c02a14fd415a6743fdb3159
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-09 14:24:09 +0100

    style fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,7 +22,6 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
-
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
